8619e9166ac5a84e362166540bdfcb79235474a2
Jeron Paraiso <jeron@kodigosystems.comgit config --global core.editor nanogit congig --global merge.tool kdiff3git config --global user.email jeron@kodigosystems.com>
2014-08-29 16:02:14 -0700

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '59674152d73416f8c2373d4a9eacb3b95cea2e28f6c375419677ad392030df17b574239b50e10f7dd982e947592a19aedb1d5d1bb2a4e8a31e19702fc5410f86'
